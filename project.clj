29e2c3846eaec678deb45746f7e40784a40e8f79
Anonymous Applicant <anonymous@example.com>
2018-04-28 16:35:52 -0400

    Add some deps

--- project.clj
+++ project.clj
@@ -1,11 +1,14 @@
 (defproject my-exercise "0.1.0-SNAPSHOT"
   :description "An anonymous Democracy Works coding exercise"
   :min-lein-version "2.7.1"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.6.3"]
-                 [ring/ring-defaults "0.3.1"]
+  ;; TODO check for deps that should be updated
+  :dependencies [[clj-http "3.8.0"]
                  [compojure "1.6.0"]
-                 [hiccup "1.0.5"]]
+                 [hiccup "1.0.5"]
+                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [ring "1.6.3"]
+                 [ring/ring-defaults "0.3.1"]]
   :plugins [[lein-ring "0.12.1"]]
   :ring {:handler my-exercise.core/handler}
   :aliases {"submit" ["run" "-m" "my-exercise.submit"]})
