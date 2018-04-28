f2f9d5af68ee976935205d57edfe78e8913904c5
Anonymous Applicant <anonymous@example.com>
2018-04-28 16:36:39 -0400

    Add minimal search page

--- src/my_exercise/search.clj
+++ src/my_exercise/search.clj
@@ -1,1 +1,23 @@
-(ns my-exercise.search)
+(ns my-exercise.search
+  (:require [clojure.tools.logging :as log]
+            [hiccup.page :refer [html5]]
+            [ring.util.anti-forgery :refer [anti-forgery-field]]))
+
+(defn election-search [request]
+  ;; TODO restart capability
+  ;; TODO state, eg mount? Probably not, can be stateless.
+  (log/warn "HELLO from logging")
+  ;; (log/warn (with-out-str (clojure.pprint/pprint request)))
+  (def rq request) ; TODO
+  [:div {:class "election-search"}
+   [:p "U R the man u so bad"]
+   ]
+
+
+  )
+
+(defn page [request]
+  (prn "YO YO from page!")
+  (html5
+   (anti-forgery-field)
+   (election-search request))) ; TODO change to [] ?
